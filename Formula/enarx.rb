cee64fc666d1808a688737a9576fe0743a4d3cd9
Paul Pietkiewicz <paul@profian.com>
2022-07-05 22:42:42 -0400

    enarx 0.6.0
    Signed-off-by: Paul Pietkiewicz <paul@profian.com>

--- Formula/enarx.rb
+++ Formula/enarx.rb
@@ -2,8 +2,8 @@ class Enarx < Formula
   desc "Confidential Computing with WASM"
   homepage "https://enarx.dev/"
   url "https://github.com/enarx/enarx.git",
-    tag:      "v0.5.1",
-    revision: "31613bdb1b13887ac9c3f4770e0a46ba3ae17be4"
+    tag:      "v0.6.0",
+    revision: "1edb0127892624a3dc5e4fd363c0f8871abf1afc"
   license "Apache-2.0"
   head "https://github.com/enarx/enarx.git", branch: "main"
 
@@ -20,7 +20,7 @@ class Enarx < Formula
     ENV.prepend_path "PATH", HOMEBREW_CACHE/"cargo_cache/bin"
     # we are using nightly because Enarx requires nightly in order to build from source
     # pinning to nightly-2021-05-03 to avoid inconstency
-    nightly_version = "nightly-2022-05-03"
+    nightly_version = "nightly-2022-06-28"
     components = %w[rust-src miri clippy rustfmt]
     system "rustup", "toolchain", "install", nightly_version
     system "rustup", "component", "add", *components, "--toolchain", nightly_version
